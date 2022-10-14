4b4a2469cf5142ab4689293231f0a8541cfa46fe
Matilda Mared <matildamared@live.se>
2022-10-14 16:58:50 +0200

    Initial Commit

---
+++ magic-8-ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic-8-ball
+//
+//  Created by Matilda Mared on 2022-10-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
